a613d2e1d322adc341564442bd8d3a2479f48ae1
akash <kumawatap63@gmail.com>
2025-10-05 18:07:45 +0530

    Added get-threads api hook

---
+++ client/src/hooks/api/get-threads.ts
@@ -0,0 +1,15 @@
+import { apiClient } from "@/lib/api-client"
+import type { ThreadType } from "@/types/threads";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchThreads = async (offset: number): Promise<ThreadType[]> => {
+  const { data } = await apiClient.get(`/chat/get-threads?offset=${offset}`)
+  return data.data;
+}
+
+export const useGetThreads = (offset: number) => {
+  return useQuery<ThreadType[], Error>({
+    queryKey: [`threads${offset}`],
+    queryFn: () => fetchThreads(offset),
+  })
+}
