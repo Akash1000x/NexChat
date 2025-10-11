20d78efdebf4abe1ebd5f6bb2d57911df2d8d0ac
akash <kumawatap63@gmail.com>
2025-10-12 00:28:46 +0530

    client: Added root page

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,14 +1,17 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
+import { TanStackDevtools } from "@tanstack/react-devtools"
 
 // import Header from '../components/Header'
 
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools"
 
-import type { QueryClient } from '@tanstack/react-query'
-import { SidebarProvider } from '@/components/ui/sidebar'
-import AppSidebar from '@/components/app-sidebar'
+import type { QueryClient } from "@tanstack/react-query"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import AppSidebar from "@/components/app-sidebar"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
+import ThemeToggle from "@/components/ui/theme-toggle"
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -16,24 +19,25 @@ interface MyRouterContext {
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
-    <>
-      {/* <Header /> */}
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SidebarProvider>
         <AppSidebar />
+        <ThemeToggle />
         <Outlet />
+        <Toaster />
       </SidebarProvider>
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'Tanstack Router',
+            name: "Tanstack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
           TanStackQueryDevtools,
         ]}
       />
-    </>
+    </ThemeProvider>
   ),
 })
